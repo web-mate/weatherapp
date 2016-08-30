ad325ae628ad1e6399d755b4e49a6baf7976fbdc
=webmate <=>
2016-08-30 12:53:45 +0300

    first commit

---
+++ weatherApplication/customLabel.swift
@@ -0,0 +1,14 @@
+//
+//  customInputField.swift
+//  weatherApplication
+//
+//  Created by dimitris vlagas on 29/8/16.
+//  Copyright © 2016 webmate. All rights reserved.
+//
+
+import UIKit
+
+class customLabel {
+    
+    
+}
