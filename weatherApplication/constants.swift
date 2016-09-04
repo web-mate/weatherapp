c8c29e5224d9488d8f0abc5c3a0d14b359d437ea
=webmate <=>
2016-09-04 09:17:30 +0300

    Added api interaction, ui ready , pods installed, and mvc files created

---
+++ weatherApplication/constants.swift
@@ -0,0 +1,13 @@
+//
+//  cosntants.swift
+//  weatherApplication
+//
+//  Created by dimitris vlagas on 31/8/16.
+//  Copyright © 2016 webmate. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://api.forecast.io/forecast/"
+let API_KEY = "340be547b1debd0c9266a06f6fb69217/"
+typealias DownloadComplete = (lat: Double, long :Double) -> ()
---
+++ weatherApplication/weather.swift
@@ -0,0 +1,9 @@
+//
+//  weather.swift
+//  weatherApplication
+//
+//  Created by dimitris vlagas on 31/8/16.
+//  Copyright © 2016 webmate. All rights reserved.
+//
+
+import Foundation
